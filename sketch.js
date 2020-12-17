bfd03be7067881752004a37091511bb524c57f9d
SanidhyaMalviya <72500621+SanidhyaMalviya@users.noreply.github.com>
2020-12-17 23:41:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+//Create variables here
+var dog,happyDog;
+var foodS,foodStock;
+var database;
+var foodObj;
+var fedTime,lastFed;
+var addFoods,feedDog;
+
+function preload(){
+  //load images here
+  dogImg = loadImage("images/dogImg.png");
+  dogImg1 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(800, 400);
+  database = firebase.database();
+  
+  foodObj = new Food();
+
+  feed = createButton("Feed the Dog");
+  feed.position(740,95);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(840,95);
+  addFood.mousePressed(addFoods);
+
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+
+  dog = createSprite(700,200);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+}
+
+function draw() {  
+  background(46,139,87);
+
+  // if(foodS !== undefined){
+  // }
+  fedTime = database.ref('feedTime');
+  fedTime.on("value",function(data){
+    lastFed = data.val();
+  });
+
+  foodObj.display();
+
+  drawSprites();
+
+  //add styles here
+  push();
+  textSize(16);
+  fill("white");
+  text("Food Remaining: "+foodS,100,30);
+  pop();
+
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed: "+ lastFed%12 + "PM",100,55);
+  } else if(lastFed == 0){
+    text("Last Feed: 12AM",100,55);
+  } else{
+    text("Last Feed: "+ lastFed + "AM",100,55);
+  }
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
+
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    food:foodS
+  })
+}
+
+function feedDog(){
+  dog.addImage(dogImg1);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    food:foodObj.getFoodStock(),
+    feedTime:hour()
+  })
+}
