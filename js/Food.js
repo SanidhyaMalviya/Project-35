307481708704a1a694f21634a3844286a842a8c5
SanidhyaMalviya <72500621+SanidhyaMalviya@users.noreply.github.com>
2020-12-18 12:58:08 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -32,13 +32,13 @@ class Food{
   display(){
     var x=80,y=100;
     imageMode(CENTER);
-    image(this.image,720,220,70,70);
+    image(this.image,630,220,70,70);
 
     if(this.foodStock!=0){
       for(var i=0; i<this.foodStock; i++){
         if(i%10 == 0){
           x = 80;
-          y = y+50;
+          y = y+60;
         }
         image(this.image,x,y,50,50);
         x = x+30;
